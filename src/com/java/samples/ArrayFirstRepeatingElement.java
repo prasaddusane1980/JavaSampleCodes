5e41baafccf32bfa1490f4de22ef6e14e3b58c52
prasaddusane1980 <prasaddusane1980@gmail.com>
2021-08-21 19:52:40 +0530

    updated logic

--- src/com/java/samples/ArrayFirstRepeatingElement.java
+++ src/com/java/samples/ArrayFirstRepeatingElement.java
@@ -1,12 +1,21 @@
 package com.java.samples;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class ArrayFirstRepeatingElement {
 
 
     public static void main(String[] args) {
-        Integer [] arr= new Integer[]{5,3,4,5,1,4,3};
+        Integer [] arr= new Integer[]{6,3,4,5,1,4,3};
+        findFirstRepetingElementUSingLinkedHashMap(arr);
+        findFirstRepetingElement(arr);
+    }
+
+    public static void findFirstRepetingElementUSingLinkedHashMap( Integer [] arr)
+    {
         LinkedHashMap<Integer,Integer> lhmp=new LinkedHashMap<Integer,Integer>();
         for (int num:arr)
         {
@@ -19,9 +28,31 @@ public class ArrayFirstRepeatingElement {
             }
         }
 
-        for (Integer key:lhmp.keySet()){
-            System.out.println("Key :"+key +"count :"+lhmp.get(key));
+        for (Map.Entry<Integer,Integer> keyVal:lhmp.entrySet()){
+            if(keyVal.getValue()>1)
+            {
+                System.out.println("First repeating element is : "+keyVal.getKey());
+                break;
+            }
         }
+    }
+
+    public static void findFirstRepetingElement( Integer [] arr){
+
+        int j=-1;
+        Set<Integer> intSet=new HashSet<>();
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            if(!intSet.contains(arr[i]))
+            {
+                intSet.add(arr[i]);
+            }else
+            {
+                j=i;
+            }
+        }
+        System.out.println("First repeating element is : "+arr[j]);
+
     }
 
 }
--- src/com/java/samples/TestHashMapWithHashCode.java
+++ src/com/java/samples/TestHashMapWithHashCode.java
@@ -31,3 +31,73 @@ public class TestHashMapWithHashCode {
     }
 
 }
+
+class TestObject {
+    int id;
+    String name;
+
+    public TestObject(int id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        if(name.contains("testObject1"))
+        {
+            return 1;
+        }
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TestObject other = (TestObject) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+}
+
