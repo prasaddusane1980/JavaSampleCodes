088662f63b24cf520e742c891e7b3a5998f8bbe6
prasaddusane1980 <prasaddusane1980@gmail.com>
2021-08-21 15:35:25 +0530

    main

--- src/com/java/samples/Main.java
+++ src/com/java/samples/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("test");
-        System.out.println("test dev1 branch");
+        System.out.println("test master branch");
     }
 }
